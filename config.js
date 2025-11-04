72d576ce2bc97f093a984bc548ee528a880ce502
sparkyterminal <shashankau17@gmail.com>
2025-11-05 00:40:37 +0530

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL = "https://www.skyblueeventmanagement.in/api/";
+export const BASE_URL = "/";
+export const SOCKET_URL = "https://www.skyblueeventmanagement.in/api";
+export const BASE_LINK = "/";
+export const SOCKET_PATH = "/";
+export const DEFAULT_IP_ADDRESS = "127.0.0.1";
